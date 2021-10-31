85af30e1cfb51e7ccf4a9366c2992900bd1f4cde
cedric thonney <c.thonney@gmail.com>
2021-10-31 16:56:34 +0400

    routes for slots

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :properties do
-    resources :spot, only: [:new, :create, :show, :edit, :update, :destroy, :index, :indexProperty]
+    resources :slots, only: [:new, :create, :edit, :update, :destroy, :indexProperty]
   end
+  resources :slots, only: [:destroy, :edit]
 end
