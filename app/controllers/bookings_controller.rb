7c7d335f8b8850253eeccba0fcf9bcf2433e110c
cedric thonney <c.thonney@gmail.com>
2021-11-09 19:04:08 +0400

    generate controller+view

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,16 @@
+class BookingsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+  get 'bookings/new'
+  get 'bookings/show'
+  get 'bookings/edit'
+  get 'bookings/delete'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
