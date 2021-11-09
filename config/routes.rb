19a11cafa399f26845367f359bc57c770b24c6de
cedric thonney <c.thonney@gmail.com>
2021-11-09 19:13:50 +0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/new'
-  get 'bookings/show'
-  get 'bookings/edit'
-  get 'bookings/delete'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :properties do
     resources :slots, only: [:new, :create, :edit, :update, :destroy, :indexProperty]
+    resources :bookings, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   end
   resources :slots, only: [:destroy, :edit]
+  resources :bookings, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
