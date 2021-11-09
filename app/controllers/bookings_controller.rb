0baf3963d6806202ba7c4a5ee7840cb1f4446439
cedric thonney <c.thonney@gmail.com>
2021-11-09 19:46:35 +0400

    methods creation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,65 @@
 class BookingsController < ApplicationController
+
   def index
+    @bookings = Booking.where(user_id: current_user.id)
+
   end
 
-  def new
+  def indexProperty
+    @bookings = Booking.where(property_id: params[:id])
   end
 
   def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+    @booking.user_id = current_user.id
+    @booking.slot_id = params[:slot_id]
+    @booking.time_slot = params[:time_slot]
+
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      flash[:notice] = "Booking was successfully created"
+      redirect_to bookings_path
+    else
+      render :new
+    end
+
   end
 
   def edit
+    @booking = Booking.find(params[:id])
+
+
   end
 
-  def delete
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      flash[:notice] = "Booking was successfully updated"
+      redirect_to bookings_path
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    flash[:notice] = "Booking was successfully deleted"
+    redirect_to bookings_path
+
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:time_slot, :user_id, :slot_id,)
   end
 end
