abf621f83a99feb58ee18cd6393bd760eb2c902f
cedric thonney <c.thonney@gmail.com>
2021-11-14 19:37:13 +0400

    delete/comment has_one

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,6 +1,6 @@
 class Slot < ApplicationRecord
   belongs_to :property
   has_many :bookings
-  has_one :owner, foreign_key: "user_id", class_name: "User", through: :properties
-  has_many :buyer, foreign_key: "user_id", class_name: "User", through: :bookings
+  # has_one :owner, foreign_key: "user_id", class_name: "User", through: :properties
+  # has_many :buyer, foreign_key: "user_id", class_name: "User", through: :bookings
 end
