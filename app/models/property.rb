4e5e29c28b59cf0b00684007a5c3664a52ebe47d
Remi Dumas <rdumas.remi@gmail.com>
2021-11-16 20:53:02 +0400

    api key mapbox dans .env

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,7 @@
 class Property < ApplicationRecord
   belongs_to :owner, foreign_key: "user_id", class_name: "User"
-  geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_location?
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   has_many :messages
   has_many :slots
   has_many :chat_rooms, through: :messages
