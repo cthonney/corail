d98fe590b1cabee5960d797f6d5cd0837a6d0fba
Remi Dumas <rdumas.remi@gmail.com>
2021-11-18 21:27:32 +0400

    carte centrée et clean

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,6 +5,8 @@ const buildMap = (mapElement) => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
+    center: [57.597165, -20.248307],
+    zoom: 9,
     style: 'mapbox://styles/mapbox/streets-v10'
   });
 };
@@ -17,13 +19,6 @@ const addMarkersToMap = (map, markers) => {
       .setPopup(popup)
       .addTo(map);
   });
-};
-
-
-const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 };
 
 
