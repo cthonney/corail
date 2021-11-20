ec98b2b2ecedd3fa70f103bc3d2b4886286f21b9
cedric thonney <c.thonney@gmail.com>
2021-11-20 15:28:21 +0400

    Update SEED

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,9 +31,18 @@ admin.avatar.attach(io: File.open("app/assets/images/avatars/avatar4.png"), file
 puts "Attached avatars to users"
 
 #Create Properties and attach them to users
-property1 = Property.create(property_type: "House", price: "100", living_space: "100", land_surface: "100", address: "Paris", latitude: "48.866667", longitude: "2.333333", parking_space: "1", pool: false, room: 4, bathroom: 5, sea_view: true, user_id: user1.id)
-property2 = Property.create(property_type: "Villa", price: "200", living_space: "100", land_surface: "100", address: "Blue-bay", latitude: "20.437257", longitude: "57.713574", parking_space: "3", pool: true, room: 3, bathroom: 2, sea_view: true, user_id: user2.id)
-property3 = Property.create(property_type: "Apartment", price: "300", living_space: "100", land_surface: "100", address: "Port Louis", latitude: "-20.166059", longitude: "57.482053", parking_space: "1", pool: false, room: 4, bathroom: 5, sea_view: true, user_id: user3.id)
+property1 = Property.create(description: "Magnifique maison sur la montagne", property_type: "House", price: "4000000", living_space: "450", land_surface: "1000", address: "Curepipe", parking_space: 1, pool: false, room: 4, bedrooms: 6, bathroom: 5, sea_view: false, user_id: user1.id)
+property2 = Property.create(description: "Magnifique Villa avec vue sur la mer", property_type: "Villa", price: "18000000", living_space: "800", land_surface: "1600", address: "Port Louis", parking_space: 3, pool: true, room: 4, bedrooms: 4, bathroom: 2, sea_view: true, user_id: user2.id)
+property3 = Property.create(description: "Magnifique Flat avec vue sur la ville", property_type: "Flat", price: "22000000", living_space: "160", land_surface: "160", address: "Blue bay", parking_space: 3, pool: true, room: 4, bedrooms: 4, bathroom: 2, sea_view: true, user_id: user3.id)
+property4 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "House", price: "10000000", living_space: "160", land_surface: "160", address: "Flic en Flac", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user1.id)
+property5 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "Villa", price: "10000000", living_space: "160", land_surface: "160", address: "Grand-baie", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user2.id)
+property6 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "Flat", price: "10000000", living_space: "160", land_surface: "160", address: "Flacq", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user3.id)
+property7 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "House", price: "10000000", living_space: "160", land_surface: "160", address: "Phoenix, Mauritius", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user1.id)
+
+
+
+
+
 
 puts "Created #{Property.count} properties"
 
@@ -44,6 +53,9 @@ puts "Created #{Property.count} properties"
 slot1 = Slot.create(date: "2021-12-01-12:00:00", property_id: property1.id)
 slot2 = Slot.create(date: "2021-12-05-13:00:00", property_id: property2.id)
 slot3 = Slot.create(date: "2021-12-10-14:00:00", property_id: property3.id)
+slot4 = Slot.create(date: "2021-12-15-15:00:00", property_id: property4.id)
+slot5 = Slot.create(date: "2021-12-20-16:00:00", property_id: property5.id)
+slot6 = Slot.create(date: "2021-12-25-17:00:00", property_id: property6.id)
 
 puts "Created #{Slot.count} slots"
 
@@ -52,7 +64,9 @@ puts "Created #{Slot.count} slots"
 booking1 = Booking.create(slot_id: slot1.id, user_id: user2.id)
 booking2 = Booking.create(slot_id: slot2.id, user_id: user3.id)
 booking3 = Booking.create(slot_id: slot3.id, user_id: user1.id)
-
+booking4 = Booking.create(slot_id: slot4.id, user_id: user2.id)
+booking5 = Booking.create(slot_id: slot5.id, user_id: user3.id)
+booking6 = Booking.create(slot_id: slot6.id, user_id: user1.id)
 puts "Created #{Booking.count} bookings"
 
 
@@ -67,6 +81,7 @@ puts "Created #{ChatRoom.count} chat rooms"
 message1 = Message.create(content: "Hello, I'm interested in your property", user_id: user1.id, property_id: property2.id, chat_room_id: chat_room1.id)
 message2 = Message.create(content: "Hello, I'm interested in your property", user_id: user2.id, property_id: property1.id, chat_room_id: chat_room2.id)
 message3 = Message.create(content: "Hello, I'm interested in your property", user_id: user3.id, property_id: property3.id , chat_room_id: chat_room3.id)
+
 
 puts "Created #{Message.count} messages"
 
