22e0cef42092b17e2a4a196a28f40fd9c090424b
Remi Dumas <rdumas.remi@gmail.com>
2021-11-20 14:31:32 +0400

    section search bar map finie

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -14,7 +14,7 @@ const buildMap = (mapElement) => {
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.info_window);
-    new mapboxgl.Marker()
+    new mapboxgl.Marker({ "color": "#E57F84" })
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup)
       .addTo(map);
