5d0b032667dbf03c64a504d165a75101c6e9ab30
cedric thonney <c.thonney@gmail.com>
2021-11-27 13:07:55 +0400

    show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,8 @@ require("channels")
 // External imports
 import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
+import { initFlickity } from '../plugins/init_flickity';
+
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
