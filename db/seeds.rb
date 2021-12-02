fb79de3fc796f0960dd807d06535960ce9f49fab
cedric thonney <c.thonney@gmail.com>
2021-12-02 17:54:35 +0400

    seed property

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,20 +33,41 @@ puts "Attached avatars to users"
 property1 = Property.create(description: "Magnifique maison sur la montagne", property_type: "House", price: "4000000", living_space: "450", land_surface: "1000", address: "Curepipe", parking_space: 1, pool: false, room: 4, bedrooms: 6, bathroom: 5, sea_view: false, user_id: user1.id)
 property2 = Property.create(description: "Magnifique Villa avec vue sur la mer", property_type: "Villa", price: "18000000", living_space: "800", land_surface: "1600", address: "Port Louis", parking_space: 3, pool: true, room: 4, bedrooms: 4, bathroom: 2, sea_view: true, user_id: user2.id)
 property3 = Property.create(description: "Magnifique Flat avec vue sur la ville", property_type: "Flat", price: "22000000", living_space: "160", land_surface: "160", address: "Blue bay", parking_space: 3, pool: true, room: 4, bedrooms: 4, bathroom: 2, sea_view: true, user_id: user3.id)
-property4 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "House", price: "10000000", living_space: "160", land_surface: "160", address: "Flic en Flac", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user1.id)
-property5 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "Villa", price: "10000000", living_space: "160", land_surface: "160", address: "Grand-baie", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user2.id)
-property6 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "Flat", price: "10000000", living_space: "160", land_surface: "160", address: "Flacq", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user3.id)
+property4 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "House", price: "23000000", living_space: "160", land_surface: "160", address: "Flic en Flac", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user1.id)
+property5 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "Villa", price: "22000000", living_space: "160", land_surface: "160", address: "Grand-baie", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user2.id)
+property6 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "Flat", price: "16000000", living_space: "160", land_surface: "160", address: "Flacq", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user3.id)
 property7 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "House", price: "10000000", living_space: "160", land_surface: "160", address: "Phoenix, Mauritius", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user1.id)
+property8 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "Villa", price: "19000000", living_space: "160", land_surface: "160", address: "Phoenix, Mauritius", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user2.id)
+property9 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "Flat", price: "10000000", living_space: "160", land_surface: "160", address: "Phoenix, Mauritius", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user3.id)
+property10 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "House", price: "20000000", living_space: "160", land_surface: "160", address: "Phoenix, Mauritius", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user1.id)
+property11 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "Villa", price: "10000000", living_space: "160", land_surface: "160", address: "Phoenix, Mauritius", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user2.id)
+property12 = Property.create(description: "Magnifique Villa avec vue sur la mer", property_type: "Villa", price: "18000000", living_space: "800", land_surface: "1600", address: "Port Louis", parking_space: 3, pool: true, room: 4, bedrooms: 4, bathroom: 2, sea_view: true, user_id: user2.id)
+property13 = Property.create(description: "Magnifique Flat avec vue sur la ville", property_type: "Flat", price: "22000000", living_space: "160", land_surface: "160", address: "Blue bay", parking_space: 3, pool: true, room: 4, bedrooms: 4, bathroom: 2, sea_view: true, user_id: user3.id)
+property14 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "House", price: "80000000", living_space: "160", land_surface: "160", address: "Flic en Flac", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user1.id)
+property15 = Property.create(description: "Magnifique Appartement avec vue sur la ville", property_type: "Villa", price: "10000000", living_space: "160", land_surface: "160", address: "Grand-baie", parking_space: 4, pool: true, room: 4, bedrooms: 2, bathroom: 1, sea_view: false, user_id: user2.id)
+
+
+
 
 puts "Created #{Property.count} properties"
 
-property1.photos.attach(io: File.open("app/assets/images/no-image.png"), filename: "no-image.png")
-property2.photos.attach(io: File.open("app/assets/images/no-image.png"), filename: "no-image.png")
-property3.photos.attach(io: File.open("app/assets/images/no-image.png"), filename: "no-image.png")
-property4.photos.attach(io: File.open("app/assets/images/no-image.png"), filename: "no-image.png")
-property5.photos.attach(io: File.open("app/assets/images/no-image.png"), filename: "no-image.png")
-property6.photos.attach(io: File.open("app/assets/images/no-image.png"), filename: "no-image.png")
-property7.photos.attach(io: File.open("app/assets/images/no-image.png"), filename: "no-image.png")
+property1.photos.attach(io: File.open("app/assets/images/seed/house1.png"), filename: "house1.png")
+property2.photos.attach(io: File.open("app/assets/images/seed/villa1.png"), filename: "villa1.png")
+property3.photos.attach(io: File.open("app/assets/images/seed/appart1.png"), filename: "appart1.png")
+property4.photos.attach(io: File.open("app/assets/images/seed/house1.png"), filename: "house1.png")
+property5.photos.attach(io: File.open("app/assets/images/seed/villa1.png"), filename: "villa1.png")
+property6.photos.attach(io: File.open("app/assets/images/seed/appart1.png"), filename: "appart1.png")
+property7.photos.attach(io: File.open("app/assets/images/seed/house1.png"), filename: "house1.png")
+property8.photos.attach(io: File.open("app/assets/images/seed/villa1.png"), filename: "villa1.png")
+property9.photos.attach(io: File.open("app/assets/images/seed/appart1.png"), filename: "appart1.png")
+property10.photos.attach(io: File.open("app/assets/images/seed/house1.png"), filename: "house1.png")
+property11.photos.attach(io: File.open("app/assets/images/seed/villa1.png"), filename: "villa1.png")
+property12.photos.attach(io: File.open("app/assets/images/seed/appart1.png"), filename: "appart1.png")
+property13.photos.attach(io: File.open("app/assets/images/seed/house1.png"), filename: "house1.png")
+property14.photos.attach(io: File.open("app/assets/images/seed/villa1.png"), filename: "villa1.png")
+property15.photos.attach(io: File.open("app/assets/images/seed/appart1.png"), filename: "appart1.png")
+
+
 
 puts "Attached photos to properties"
 
@@ -60,6 +81,23 @@ slot3 = Slot.create(date: "2021-12-10-14:00:00", property_id: property3.id)
 slot4 = Slot.create(date: "2021-12-15-15:00:00", property_id: property4.id)
 slot5 = Slot.create(date: "2021-12-20-16:00:00", property_id: property5.id)
 slot6 = Slot.create(date: "2021-12-25-17:00:00", property_id: property6.id)
+slot7 = Slot.create(date: "2021-12-30-18:00:00", property_id: property7.id)
+slot8 = Slot.create(date: "2022-01-04-19:00:00", property_id: property8.id)
+slot9 = Slot.create(date: "2022-01-09-20:00:00", property_id: property9.id)
+slot10 = Slot.create(date: "2022-01-14-21:00:00", property_id: property10.id)
+slot11 = Slot.create(date: "2022-01-19-22:00:00", property_id: property11.id)
+slot12 = Slot.create(date: "2022-01-24-23:00:00", property_id: property12.id)
+slot13 = Slot.create(date: "2022-01-29-00:00:00", property_id: property13.id)
+slot14 = Slot.create(date: "2022-02-03-01:00:00", property_id: property14.id)
+slot15 = Slot.create(date: "2022-02-08-02:00:00", property_id: property15.id)
+
+
+puts "Created #{Slot.count} slots"
+
+puts "Attached slots to properties"
+
+#Create Bookings
+booking1 = Booking
 
 puts "Created #{Slot.count} slots"
 
@@ -71,18 +109,23 @@ booking3 = Booking.create(slot_id: slot3.id, user_id: user1.id)
 booking4 = Booking.create(slot_id: slot4.id, user_id: user2.id)
 booking5 = Booking.create(slot_id: slot5.id, user_id: user3.id)
 booking6 = Booking.create(slot_id: slot6.id, user_id: user1.id)
+booking7 = Booking.create(slot_id: slot7.id, user_id: user2.id)
+booking8 = Booking.create(slot_id: slot8.id, user_id: user3.id)
+booking9 = Booking.create(slot_id: slot9.id, user_id: user1.id)
+booking10 = Booking.create(slot_id: slot10.id, user_id: user2.id)
+booking11 = Booking.create(slot_id: slot11.id, user_id: user3.id)
+booking12 = Booking.create(slot_id: slot12.id, user_id: user1.id)
+booking13 = Booking.create(slot_id: slot13.id, user_id: user2.id)
+booking14 = Booking.create(slot_id: slot14.id, user_id: user3.id)
+booking15 = Booking.create(slot_id: slot15.id, user_id: user1.id)
+
+
 puts "Created #{Booking.count} bookings"
 
-#Create messages
+puts "***SEED DONE***"
 
-chat_room1 = ChatRoom.create(title: user1.name)
-chat_room2 = ChatRoom.create(title: user2.name)
-chat_room3 = ChatRoom.create(title: user3.name)
 
-puts "Created #{ChatRoom.count} chat rooms"
 
-message1 = Message.create(content: "Hello, I'm interested in your property", user_id: user1.id, property_id: property2.id, chat_room_id: chat_room1.id)
-message2 = Message.create(content: "Hello, I'm interested in your property", user_id: user2.id, property_id: property1.id, chat_room_id: chat_room2.id)
-message3 = Message.create(content: "Hello, I'm interested in your property", user_id: user3.id, property_id: property3.id, chat_room_id: chat_room3.id)
 
-puts "Created #{Message.count} messages"
+
+
